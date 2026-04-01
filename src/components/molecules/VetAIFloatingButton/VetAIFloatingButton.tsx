ce0e52426f330ca67c9fec4a90b2e1983ebb4bb4
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2026-04-01 14:12:48 +0700

    feat: enhance VetAI chat functionality with session management
    - Integrated session management for VetAI chat, allowing users to retrieve and save conversation history.
    - Added a conversation divider to improve chat message organization.
    - Updated VetAIFloatingButton positioning for better responsiveness across devices.

--- src/components/molecules/VetAIFloatingButton/VetAIFloatingButton.tsx
+++ src/components/molecules/VetAIFloatingButton/VetAIFloatingButton.tsx
@@ -3,7 +3,7 @@ import { VetAIIcon } from "@/icons"
 
 export const VetAIFloatingButton = () => {
   return (
-    <div className="fixed bottom-16 right-16">
+    <div className="fixed bottom-4 md:bottom-8 lg:bottom-16 right-4 md:right-8 lg:right-16">
       <LocalizedClientLink href="/vet-ai">
         <div className="flex items-center justify-center gap-0.5 flex-col sop-gradient-01 rounded-tl-4xl rounded-bl-4xl rounded-tr-4xl rounded-br-sm aspect-square h-sop-64px">
           <VetAIIcon size={30} color="#fff" />
