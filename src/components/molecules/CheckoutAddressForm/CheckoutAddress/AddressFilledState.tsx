47f6a0c5d670dc83638d2493501f10b844272f8e
Tanatorn Suteerapornchai <132776555+atnts@users.noreply.github.com>
2026-05-17 19:40:07 +0700

    feat: add mobile address display logic and utility for mobile detection

--- src/components/molecules/CheckoutAddressForm/CheckoutAddress/AddressFilledState.tsx
+++ src/components/molecules/CheckoutAddressForm/CheckoutAddress/AddressFilledState.tsx
@@ -2,6 +2,7 @@ import { Infotag } from "@/components/atoms/InfoTag/Infotag"
 import { Button } from "@/components/atoms"
 import { Dot } from "lucide-react"
 import { StoreCustomer, StoreCustomerAddress } from "@medusajs/types"
+import { isMobile } from "@/lib/utils/is-mobile"
 
 const AddressFilledState = ({
   customer,
@@ -13,14 +14,39 @@ const AddressFilledState = ({
   )
 
   const fullAddress = `${address?.address_1} ${address?.address_2} ${address?.city} ${address?.province} ${address?.postal_code}`
-  return (
+
+  return isMobile() ? (
+    <div className="flex gap-3">
+      <div className="pr-5.5">
+        <div className="flex flex-col">
+          <div className="flex gap-2">
+            <label>{address?.address_name}</label>
+            <label>({address?.phone})</label>
+          </div>
+          <label className="sop-body-md-regular">{fullAddress}</label>
+          <div className="item-center justify-between flex mt-sop-16px">
+            <Infotag
+              className="sop-body-sm-medium bg-sop-secondary-100 text-sop-secondary-500 rounded-sop-16 pr-2.5"
+              leftIcon={<Dot size={32} />}
+            >
+              ค่าเริ่มต้น
+            </Infotag>
+            <Button type="submit" variant="outline" size="xl" rounded="rounded">
+              เปลี่ยน
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
     <div className="flex justify-between items-center">
       <div className="pr-5.5">
         <div className="flex items-center gap-4">
-          <div className="flex  gap-2">
-            <label> {customer?.addresses?.[0].address_name}</label>
-            <label>({customer?.addresses?.[0].phone})</label>
+          <div className="flex gap-2">
+            <label>{address?.address_name}</label>
+            <label>({address?.phone})</label>
           </div>
+
           <Infotag
             className="sop-body-sm-medium bg-sop-secondary-100 text-sop-secondary-500 rounded-sop-16 pr-2.5"
             leftIcon={<Dot size={32} />}
@@ -28,8 +54,10 @@ const AddressFilledState = ({
             ค่าเริ่มต้น
           </Infotag>
         </div>
+
         <label className="sop-body-md-regular">{fullAddress}</label>
       </div>
+
       <Button type="submit" variant="outline" size="xl" rounded="rounded">
         เปลี่ยน
       </Button>
